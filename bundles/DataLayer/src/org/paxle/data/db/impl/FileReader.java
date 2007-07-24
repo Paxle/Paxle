375d38ebfff688f583235460cbcbed73840eacf4
Martin Thelian <martin.thelian@gmail.com>
2007-07-24 16:26:20 +0000

    *) reading multiple commands from file
    git-svn-id: svn://svn.paxle.net/paxle/trunk@95 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/DataLayer/src/org/paxle/data/db/impl/FileReader.java
+++ bundles/DataLayer/src/org/paxle/data/db/impl/FileReader.java
@@ -1,12 +1,14 @@
 package org.paxle.data.db.impl;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.exolab.castor.mapping.Mapping;
 import org.exolab.castor.xml.Unmarshaller;
 import org.paxle.core.data.IDataProvider;
 import org.paxle.core.data.IDataSink;
 import org.paxle.core.queue.Command;
+import org.paxle.core.queue.ICommand;
 import org.xml.sax.InputSource;
 
 public class FileReader extends Thread implements IDataProvider {
@@ -54,19 +56,18 @@ public class FileReader extends Thread implements IDataProvider {
     		Mapping mapping = new Mapping();
     		mapping.loadMapping(this.getClass().getResource("/resources/castor/mapping_command.xml"));        	
         	
-			Unmarshaller unmarshaller = new Unmarshaller(Command.class);
+			Unmarshaller unmarshaller = new Unmarshaller(ArrayList.class);
 			unmarshaller.setMapping(mapping);
-			Command command = (Command) unmarshaller.unmarshal(new InputSource(inputStream));
-						
-			// TODO: support for multiple commands required
-			
-            this.addCommand(command);
+			ArrayList commands = (ArrayList) unmarshaller.unmarshal(new InputSource(inputStream));
+			for (ICommand command : (ArrayList<ICommand>)commands) {
+				this.addCommand(command);
+			}
         } catch (Exception e) {
             e.printStackTrace();
         }    
     }	
     
-    public void addCommand(Command nextCommand) throws Exception {
+    public void addCommand(ICommand nextCommand) throws Exception {
     	this.sink.putData(nextCommand);
     }
 }
