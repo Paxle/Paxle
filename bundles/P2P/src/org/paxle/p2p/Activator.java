f46e7821e0011a29d308f8860163390117492c00
Martin Thelian <martin.thelian@gmail.com>
2007-07-30 04:48:04 +0000

    *) required libraries added    TODO: XML libs should be shifted into a sperarate package later *) usage of deprecated function removed *) running the P2P bundle should work now if following property is set in the launche configuration:    org.osgi.framework.system.packages=javax.security.cert,sun.reflect
    git-svn-id: svn://svn.paxle.net/paxle/trunk@133 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/P2P/src/org/paxle/p2p/Activator.java
+++ bundles/P2P/src/org/paxle/p2p/Activator.java
@@ -3,8 +3,11 @@ package org.paxle.p2p;
  * Created on Fri Jul 27 18:27:15 GMT+02:00 2007
  */
 
+import java.io.File;
+
 import net.jxta.peergroup.PeerGroup;
 import net.jxta.peergroup.PeerGroupFactory;
+import net.jxta.platform.NetworkManager;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -14,13 +17,29 @@ import com.axlight.jnushare.gisp.ResultListener;
 
 public class Activator implements BundleActivator {
   
-  /* (non-Javadoc)
-   * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-   */
+	/**
+	 * ATTENTION: Set the property value of
+	 * <code>org.osgi.framework.system.packages</code>
+	 * to <code>javax.security.cert,sun.reflect</code>
+	 * to run the following code:	
+	 */
   public void start(BundleContext context) throws Exception {
-	  //EXAMPLE FROM: https://gisp.dev.java.net/
-	  PeerGroup group = PeerGroupFactory.newNetPeerGroup();
+//	  //EXAMPLE FROM: https://gisp.dev.java.net/
+//	  PeerGroup group = PeerGroupFactory.newNetPeerGroup();
 
+	  NetworkManager manager = null;
+      try {
+          manager = new NetworkManager(NetworkManager.ConfigMode.EDGE, "DiscoveryServer"
+                  ,
+                  new File(new File(".cache"), "DiscoveryServer").toURI());
+          manager.startNetwork();
+      } catch (Exception e) {
+          e.printStackTrace();
+          System.exit(-1);
+      }
+      PeerGroup group = manager.getNetPeerGroup();	  
+	  
+	  
 	  GISPImpl gisp = new GISPImpl();
 	  gisp.init(group, null, null);
 	  gisp.startApp(null);
