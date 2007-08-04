f3547459b42878e7d910067ddf90943fc2994db9
Franz Brauße <dev@karlchenofhell.org>
2007-08-04 11:31:09 +0000

    - enhanced bundle view a bit and it doesn't show the system-bundle anymore
    git-svn-id: svn://svn.paxle.net/paxle/trunk@184 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/GUI/src/org/paxle/gui/impl/BundleView.java
+++ bundles/GUI/src/org/paxle/gui/impl/BundleView.java
@@ -1,15 +1,31 @@
 package org.paxle.gui.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.context.Context;
+
 import org.osgi.framework.Bundle;
 
-
 public class BundleView extends AServlet {
-
+	
+	private static final long serialVersionUID = 1L;
+	
+    private static final Map<Integer,String> states = new HashMap<Integer,String>();
+    static {
+    	states.put(Bundle.ACTIVE, "active");
+    	states.put(Bundle.INSTALLED, "installed");
+    	states.put(Bundle.RESOLVED, "resolved");
+    	states.put(Bundle.STARTING, "starting");
+    	states.put(Bundle.STOPPING, "stopping");
+    	states.put(Bundle.UNINSTALLED, "uninstalled");
+    }
+	
 	public BundleView(ServiceManager manager) {
 		super(manager);
 	}	
@@ -17,20 +33,36 @@ public class BundleView extends AServlet {
     public Template handleRequest( HttpServletRequest request,
             HttpServletResponse response,
             Context context ) {
-
+    	
         Template template = null;
-        
         try {
+            template = this.getTemplate("/resources/templates/bundle.vm");
             if (request.getParameter("update") != null) {
                 Bundle bundle = this.manager.getBundle(Long.valueOf(request.getParameter("bundleID")));
                 bundle.update();
+            } else if (request.getParameter("start") != null) {
+                Bundle bundle = this.manager.getBundle(Long.valueOf(request.getParameter("bundleID")));
+                bundle.start();
+            } else if (request.getParameter("stop") != null) {
+                Bundle bundle = this.manager.getBundle(Long.valueOf(request.getParameter("bundleID")));
+                bundle.stop();
             }
-            context.put("manager", this.manager);             
-            template = this.getTemplate("/resources/templates/bundle.vm");
-        } catch( Exception e ) {
+            
+            context.put("manager", this.manager);            
+            context.put("bundles", bundles2map(this.manager.getBundles()));
+            context.put("states", states);
+        } catch (Exception e) {
             System.err.println("Exception caught: " + e.getMessage());
         }
         
         return template;
+    }
+    
+    private static TreeMap<Long,Bundle> bundles2map(Bundle[] bundles) {
+    	final TreeMap<Long,Bundle> r = new TreeMap<Long,Bundle>();
+    	for (Bundle bundle : bundles)
+    		if (bundle.getBundleId() > 0)
+    			r.put(bundle.getBundleId(), bundle);
+    	return r;
     }
 }
