91d67c74e91f4cfe5c744f7844bebd86a4d8d040
Matthias Söhnholz <miniminko@googlemail.com>
2007-08-16 07:28:14 +0000

    *) added "Last Crawled" to search-results
    git-svn-id: svn://svn.paxle.net/paxle/trunk@221 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/GUI/src/org/paxle/gui/impl/SearchView.java
+++ bundles/GUI/src/org/paxle/gui/impl/SearchView.java
@@ -1,5 +1,8 @@
 package org.paxle.gui.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,11 +10,19 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.velocity.Template;
 import org.apache.velocity.context.Context;
+import org.paxle.core.doc.Field;
 import org.paxle.core.doc.IIndexerDocument;
 
 public class SearchView extends AServlet {
 
     protected Log logger = LogFactory.getLog(SearchView.class);
+    
+    private static final Map<String,Field<?>> fields = new HashMap<String,Field<?>>();
+    static {
+        fields.put("URL", IIndexerDocument.LOCATION);
+        fields.put("TITLE", IIndexerDocument.TITLE);
+        fields.put("LAST_CRAWLED", IIndexerDocument.LAST_CRAWLED);
+    }
     
     public SearchView(ServiceManager manager) {
         super(manager);
@@ -28,8 +39,7 @@ public class SearchView extends AServlet {
                 context.put("searchQuery", request.getParameter("query"));
             }
             context.put("manager", this.manager);
-            context.put("url", IIndexerDocument.LOCATION);
-            context.put("title", IIndexerDocument.TITLE);
+            context.put("fields", fields);
             template = this.getTemplate("/resources/templates/SearchView.vm");
             
         } catch (Exception e) {
