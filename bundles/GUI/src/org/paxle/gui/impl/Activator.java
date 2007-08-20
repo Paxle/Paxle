e71a6ef4a1ee44623cd9e8f019db99fc5813305b
Martin Thelian <martin.thelian@gmail.com>
2007-08-20 05:57:00 +0000

    *) adding a hint how user-authentication could be done for our GUI serlets
    git-svn-id: svn://svn.paxle.net/paxle/trunk@243 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/GUI/src/org/paxle/gui/impl/Activator.java
+++ bundles/GUI/src/org/paxle/gui/impl/Activator.java
@@ -1,10 +1,15 @@
 package org.paxle.gui.impl;
 
+import java.net.URL;
 import java.util.Hashtable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpContext;
 import org.osgi.service.http.HttpService;
 
 public class Activator implements BundleActivator {
@@ -27,14 +32,44 @@ public class Activator implements BundleActivator {
 				props.put("org.apache.velocity.properties", "/resources/velocity.properties");
 				props.put("bundle.location",context.getBundle().getLocation());
 				
+				HttpContext httpContext = null;
+ 
+				/* TODO: do user authentication here.
+				 * Please read http://www2.osgi.org/javadoc/r4/index.html to see how this could be used in combination 
+				 * with the OSGI User Admin Service (http://www2.osgi.org/javadoc/r4/org/osgi/service/useradmin/package-summary.html)
+				 */
+//				httpContext = new HttpContext() {
+//					/**
+//					 * @see <a href="http://www2.osgi.org/javadoc/r4/index.html">HttpContext.handleSecurity</a>
+//					 */
+//					public boolean handleSecurity(HttpServletRequest  request, HttpServletResponse response) 
+//					throws java.io.IOException {
+//						// TODO: authenticate the user here ...
+//						if (false) {
+//							response.setStatus(401);
+//							response.setHeader("WWW-Authenticate", "Basic realm=\"paxle log-in\"");
+//							return false;
+//						}
+//						return true;
+//					}
+//
+//					public String getMimeType(String arg0) {
+//						return null;
+//					}
+//
+//					public URL getResource(String arg0) {
+//						return bc.getBundle().getResource(arg0);
+//					}	
+//				};
+				
 				// registering the servlet which will be accessible using 
-				http.registerServlet("/status", new StatusView(manager), props, null);
-                http.registerServlet("/crawler", new CrawlerView(manager), props, null);
-                http.registerServlet("/bundle", new BundleView(manager), props, null);
-                http.registerServlet("/log", new LogView(manager), props, null);
-                http.registerServlet("/", new SearchView(manager), props, null);
-                http.registerServlet("/index.html", new SearchView(manager), props, null);
-                http.registerServlet("/search", new SearchView(manager), props, null);
+				http.registerServlet("/status", new StatusView(manager), props, httpContext);
+                http.registerServlet("/crawler", new CrawlerView(manager), props, httpContext);
+                http.registerServlet("/bundle", new BundleView(manager), props, httpContext);
+                http.registerServlet("/log", new LogView(manager), props, httpContext);
+                http.registerServlet("/", new SearchView(manager), props, httpContext);
+                http.registerServlet("/index.html", new SearchView(manager), props, httpContext);
+                http.registerServlet("/search", new SearchView(manager), props, httpContext);
 
 			}
 		}		
