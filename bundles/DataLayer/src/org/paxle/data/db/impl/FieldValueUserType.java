b689251a6821b160b5d60237b3bb1edb4dec516d
Martin Thelian <martin.thelian@gmail.com>
2007-08-28 16:25:12 +0000

    *) new UserType for serializeable field value    - this class is required for better error-handling
    git-svn-id: svn://svn.paxle.net/paxle/trunk@286 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/DataLayer/src/org/paxle/data/db/impl/FieldValueUserType.java
@@ -0,0 +1,28 @@
+package org.paxle.data.db.impl;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.type.SerializableType;
+import org.hibernate.type.SerializationException;
+
+public class FieldValueUserType extends SerializableType {
+	private Log logger = LogFactory.getLog(this.getClass().getName());
+	
+	public FieldValueUserType() {
+		super(Serializable.class);
+	}
+
+	public Object get(ResultSet rs, String name) throws HibernateException, SQLException {
+		try {
+			return super.get(rs, name);
+		} catch (SerializationException s) {
+			this.logger.error(String.format("Error while loading deserializing field value. The SQL statement was:\r\n%s", rs.getStatement()),s);
+			return null;
+		}
+	}
+}
