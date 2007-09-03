5dff89d55a3b37172afdadfb3bbeb5077c6f8d45
Martin Thelian <martin.thelian@gmail.com>
2007-09-03 04:50:04 +0000

    *) code cleanup *) sub-crawlers can now set the ISubCrawler.PROP_PROTOCOL property as String[]
    git-svn-id: svn://svn.paxle.net/paxle/trunk@321 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/CrawlerCore/src/org/paxle/crawler/impl/SubCrawlerListener.java
+++ bundles/CrawlerCore/src/org/paxle/crawler/impl/SubCrawlerListener.java
@@ -1,6 +1,7 @@
 package org.paxle.crawler.impl;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
@@ -13,8 +14,8 @@ public class SubCrawlerListener implements ServiceListener {
 	/**
 	 * A LDAP styled expression used for the service-listener
 	 */
-	public static String FILTER = "(& (objectClass=" + ISubCrawler.class.getName () +") "+
-	 							     "(" + ISubCrawler.PROP_PROTOCOL + "=*))";	
+	public static String FILTER = String.format("(&(%s=%s)(%s=*))",
+			Constants.OBJECTCLASS, ISubCrawler.class.getName(), ISubCrawler.PROP_PROTOCOL);	
 	
 	/**
 	 * A class to manage {@link ISubCrawler sub-crawlers}
@@ -26,46 +27,41 @@ public class SubCrawlerListener implements ServiceListener {
 	 */	
 	private BundleContext context = null;
 	
-	public SubCrawlerListener(SubCrawlerManager manager, BundleContext context) {
+	public SubCrawlerListener(SubCrawlerManager manager, BundleContext context) throws InvalidSyntaxException {
 		this.manager = manager;
 		this.context = context;
-		try {
-			ServiceReference[] refs = context.getServiceReferences(ISubCrawler.class.getName(),"(" + ISubCrawler.PROP_PROTOCOL + "=*)");
-			if (refs == null) return;
-			for (ServiceReference ref : refs) {
-				// the protocol supported by the detected sub-crawler
-				String protocol = (String) ref.getProperty(ISubCrawler.PROP_PROTOCOL);				
-				
-				// get the subcrawler
-				ISubCrawler subCrawler = (ISubCrawler) this.context.getService(ref);
-				
-				// pass it to the subCrawlerManager
-				manager.addSubCrawler(protocol, subCrawler);
-			}
-		} catch (InvalidSyntaxException e) {
-			e.printStackTrace();
-		}
+		
+		ServiceReference[] services = context.getServiceReferences(null,FILTER);
+		if (services != null) for (ServiceReference service : services) serviceChanged(service, ServiceEvent.REGISTERED);	
 	}
 	
 	/**
 	 * @see ServiceListener#serviceChanged(ServiceEvent)
-	 */	
+	 */
 	public void serviceChanged(ServiceEvent event) {
+		// getting the service reference
 		ServiceReference reference = event.getServiceReference();
+		this.serviceChanged(reference, event.getType());
+	}		
+	
+	private void serviceChanged(ServiceReference reference, int eventType) {
+		if (reference == null) return;		
 		
 		// the protocol supported by the detected sub-crawler
-		String protocol = (String) reference.getProperty(ISubCrawler.PROP_PROTOCOL);
+		String[] protocols = null;
+		Object tmp = reference.getProperty(ISubCrawler.PROP_PROTOCOL);
+		if (tmp instanceof String) protocols = new String[]{(String)tmp};
+		else if (tmp instanceof String[]) protocols = (String[])tmp;
 		
-		int eventType = event.getType();
 		if (eventType == ServiceEvent.REGISTERED) {			
 			// a reference to the service
 			ISubCrawler subCrawler = (ISubCrawler) this.context.getService(reference);			
 			
 			// new service was installed
-			manager.addSubCrawler(protocol, subCrawler);
+			manager.addSubCrawler(protocols, subCrawler);
 		} else if (eventType == ServiceEvent.UNREGISTERING) {
 			// service was uninstalled
-			manager.removeSubCrawler(protocol);
+			manager.removeSubCrawler(protocols);
 		} else if (eventType == ServiceEvent.MODIFIED) {
 			// service properties have changed
 		}		
--- bundles/CrawlerCore/src/org/paxle/crawler/impl/SubCrawlerManager.java
+++ bundles/CrawlerCore/src/org/paxle/crawler/impl/SubCrawlerManager.java
@@ -4,11 +4,15 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.ServiceReference;
 import org.paxle.crawler.ISubCrawler;
 import org.paxle.crawler.ISubCrawlerManager;
 
 public class SubCrawlerManager implements ISubCrawlerManager {
+	
+	private Log logger = LogFactory.getLog(this.getClass());
 	
 	/**
 	 * A {@link HashMap} containing the protocol that is supported by the sub-crawler as key and
@@ -18,21 +22,30 @@ public class SubCrawlerManager implements ISubCrawlerManager {
 
 	/**
 	 * Adds a newly detected {@link ISubCrawler} to the {@link Activator#subCrawlerList subcrawler-list}
+	 * @param protocols the protocols supported by the crawler
 	 * @param reference the reference to the deployed {@link ISubCrawler subcrawler-service}
 	 */
-	public void addSubCrawler(String protocol, ISubCrawler subCrawler) {
+	public void addSubCrawler(String[] protocols, ISubCrawler subCrawler) {
+		for (String protocol : protocols) this.addSubCrawler(protocol, subCrawler);
+	}
+	
+	private void addSubCrawler(String protocol, ISubCrawler subCrawler) {
 		this.subCrawlerList.put(protocol, subCrawler);		
-		System.out.println("Crawler for protocol '" + protocol + "' was installed.");
+		this.logger.info(String.format("Crawler for protocol '%s' was installed.",protocol));
 	}
 	
 	/**
 	 * Removes a uninstalled {@link ISubCrawler} from the {@link Activator#subCrawlerList subcrawler-list}
-	 * @param reference the reference to the uninstalled {@link ISubCrawler subcrawler-service}
+	 * @param protocols the protocols supported by the crawler that should be uninstalled
 	 */
+	public void removeSubCrawler(String[] protocols) {
+		for (String protocol : protocols) this.removeSubCrawler(protocol);
+	}	
+	
 	public void removeSubCrawler(String protocol) {
 		this.subCrawlerList.remove(protocol);
-		System.out.println("Crawler for protocol '" + protocol + "' was uninstalled.");
-	}	
+		this.logger.info(String.format("Crawler for protocol '%s' was uninstalled.",protocol));
+	}		
 	
 	/**
 	 * Getting a {@link ISubCrawler} which is capable to handle
