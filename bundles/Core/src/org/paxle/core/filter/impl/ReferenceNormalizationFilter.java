c83dfe28b7469faf47395e2e594c7d571a5b7c8d
Roland Ramthun <mail@roland-ramthun.de>
2007-10-16 15:29:23 +0000

    *) Normalization now adds a trailing slash if necessary
    You can now begin to use it, as I will complete it (slowly).
    
    git-svn-id: svn://svn.paxle.net/paxle/trunk@413 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Core/src/org/paxle/core/filter/impl/ReferenceNormalizationFilter.java
+++ bundles/Core/src/org/paxle/core/filter/impl/ReferenceNormalizationFilter.java
@@ -15,17 +15,107 @@ public class ReferenceNormalizationFilter implements IFilter {
 	
 	}
 	
-	public String normalizeLocation(String location) {
-		// TODO: resolve backpath
+	/**
+	 * For testing purpose only
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		
-		// TODO: undecode encoded chars
+		normalizeLocation("http://www.heise.de:777");
 		
-		// TODO: order dynamic parameters
+		System.exit(0);
+	}
+	
+	/**
+	 * This method takes an URL as input and returns it in a normalized form. There should be no change in the functionality of the URL.
+	 * This function is not speed optimized yet, as safety is more important than speed.
+	 * @param location the unnormalized URL
+	 * @return the normalized URL
+	 * @author Roland Ramthun
+	 */
+	public static String normalizeLocation(String location) {
 		
-		// TODO: what about hostnames / IP-adresses?
+		System.out.println("Started normalization of URL: '" + location + "'");
 		
+		String newloc = location;
 		
-		// TODO:
+		/*
+		 * Description: Add trailing slash to URL
+		 * 
+		 * Assumptions:
+		 * 1. A directory has to be denoted with a trailing slash (e.g. "http://paxle.net/pictures/")
+		 * 2. Every URL has to end with a slash, if it only consists of a hostname (this is in fact a directory on a server).
+		 * 3. If the string contains no single slash ("/"), there must be added the trailing slash.
+		 * 
+		 * Requirements:
+		 * 1. This step doesn't require any prior work on the string
+		 * 
+		 * Example:
+		 * "http://paxle.net"-->"http://paxle.net/"
+		 */
+		
+		if (!(location.lastIndexOf("/") > location.lastIndexOf("//") + 1)) {
+			System.out.print("'" + newloc + "'" + " --> '");
+			newloc += "/";
+			System.out.println(newloc + "'");
+		}
+		
+		/*
+		 * Description: Remove unnecessary ports from URL
+		 * These are:
+		 * 80 for HTTP
+		 * 443 for HTTPS
+		 * 21 for FTP
+		 * 22 for Telnet
+		 * 23 for SSH
+		 * 
+		 * Assumptions: 
+		 * 1. The port is the first number in the string
+		 * 2. The port has a leading substring ":"
+		 * 3. The port is followed by a slash
+		 * 4. "Add trailing slash" has to be done before this step, as assumption 3 would be wrong otherwise
+		 * 
+		 *  Example:
+		 *  "http://paxle.net:80/download.htm" --> "http://paxle.net/download.htm"
+		 */
+		
+		//TO-DO: Check port range and check for trailing slash
+		//newloc = newloc.replaceFirst(":[0-9]{1,5}", "");
+		
+		/*
+		 * Convert scheme and hostname to lowercase
+		 */
+		
+		/*
+		 * Capitalize letters in escape sequences
+		 */
+		
+		/*
+		 * Remove .. and . from URL
+		 */
+		
+		/*
+		 * Reorder GET-parameters alphabetically: http://paxle.net/content?lang=en&article=URL --> http://www.paxle.net/content?article=URL&lang=en
+		 */
+		
+		/*
+		 * Remove the fragment.
+		 */
+		
+		/*
+		 * Convert URL to lowercase, after retrieving lower- and uppercase version of the site.
+		 * This has to be discussed.
+		 */
+		
+		/*
+		 * Add or remove www in front of hostname after checking for the same content
+		 * This has to be discussed.
+		 */
+		
+		// TODO: see Bugtracker
+
+		System.out.println("Finished normalization of URL: '" + location + "' --> '" + newloc + "'");
+		
 		return location;
 	}
 	
