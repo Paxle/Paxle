6685821353c1752438acae2189dcfaa6ed222771
Martin Thelian <martin.thelian@gmail.com>
2007-11-02 23:42:43 +0000

    *) installed search-providers are now displayed on GUI
    git-svn-id: svn://svn.paxle.net/paxle/trunk@440 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/SearchEngine/src/org/paxle/se/search/ISearchProviderManager.java
+++ bundles/SearchEngine/src/org/paxle/se/search/ISearchProviderManager.java
@@ -1,9 +1,15 @@
 package org.paxle.se.search;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public interface ISearchProviderManager {
+	/**
+	 * @return the list of installed {@link ISearchProvider search-providers}
+	 */
+	public Collection<ISearchProvider> getSearchProviders();
+	
 	public List<ISearchResult> search(String paxleQuery, int maxResults, long timeout) throws
 			InterruptedException, ExecutionException;
 	public void search(String paxleQuery, int maxResults, long timeout, ISearchResultCollector collector) throws
