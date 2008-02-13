9ff583f94ed5916ea138c39e4fe79e2257b84895
Matthias Söhnholz <miniminko@googlemail.com>
2008-02-13 22:32:27 +0000

    *) added build-file for HttpTools-Bundle
    git-svn-id: svn://svn.paxle.net/paxle/trunk@523 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/HttpTools/src/org/paxle/httptools/Activator.java
@@ -0,0 +1,18 @@
+package org.paxle.httptools;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
