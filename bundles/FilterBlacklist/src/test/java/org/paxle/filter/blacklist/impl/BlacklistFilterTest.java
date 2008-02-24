c63a2ac21ec9307c048187f731929859b8ad8e24
Matthias Söhnholz <miniminko@googlemail.com>
2008-02-24 21:54:42 +0000

    *) added jUnit-tests for BlacklistFilter (not complete)
    git-svn-id: svn://svn.paxle.net/paxle/trunk@597 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/FilterBlacklist/src/test/java/org/paxle/filter/blacklist/impl/BlacklistFilterTest.java
@@ -0,0 +1,53 @@
+package org.paxle.filter.blacklist.impl;
+
+import java.io.File;
+
+import org.paxle.core.queue.Command;
+import org.paxle.core.queue.ICommand;
+
+import junit.framework.TestCase;
+
+public class BlacklistFilterTest extends TestCase {
+
+	private File testDir = null;
+	private BlacklistFilter testFilter;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		testDir = new File("testDir");
+		testDir.mkdir();
+		testDir.deleteOnExit();
+		System.out.println(testDir.getAbsolutePath());
+		new File(testDir, "testList").createNewFile();
+		testFilter = new BlacklistFilter(testDir);
+	}
+	
+	public void testIsListed() throws Exception {
+		ICommand testCommand = new Command();
+		testCommand.setLocation("http://test/");
+		testFilter.filter(testCommand, null);
+		assertTrue(testCommand.getResult().equals(ICommand.Result.Passed));
+		testCommand.setLocation("http://asd/");
+		testFilter.filter(testCommand, null);
+		assertTrue(testCommand.getResult().equals(ICommand.Result.Passed));
+		BlacklistFilter.addPattern(".*asd.*", "testList");
+		testCommand.setLocation("http://test/");
+		testFilter.filter(testCommand, null);
+		assertTrue(testCommand.getResult().equals(ICommand.Result.Passed));
+		testCommand.setLocation("http://asd/");
+		testFilter.filter(testCommand, null);
+		assertTrue(testCommand.getResult().equals(ICommand.Result.Rejected));
+		
+	}
+
+	public void testAddList() throws Exception {
+		assertFalse(new File(testDir,"testList2").exists());
+		BlacklistFilter.addList("testList2");
+		assertTrue(new File(testDir,"testList2").exists());
+	}
+	
+	protected void tearDown() throws Exception {
+		new File(testDir,"testList").delete();
+		new File(testDir,"testList2").delete();
+	}
+}
