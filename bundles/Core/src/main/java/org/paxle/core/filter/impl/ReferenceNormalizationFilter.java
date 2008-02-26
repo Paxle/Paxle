d2225dff9e07365b4b28e6336b463a0866b71027
Roland Ramthun <mail@roland-ramthun.de>
2008-02-26 13:22:07 +0000

    *) Finished standard URL normalization. You can now use it.
    git-svn-id: svn://svn.paxle.net/paxle/trunk@622 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Core/src/main/java/org/paxle/core/filter/impl/ReferenceNormalizationFilter.java
+++ bundles/Core/src/main/java/org/paxle/core/filter/impl/ReferenceNormalizationFilter.java
@@ -1,5 +1,8 @@
 package org.paxle.core.filter.impl;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,113 +12,159 @@ import org.paxle.core.queue.ICommand;
 
 public class ReferenceNormalizationFilter implements IFilter {
 
+	private static final int PORT = 1;
+	private static final int PROTOCOL = 2;
+	private static final int HOST = 3;
+	private static final int QUERY = 4;
+	private static final int PATH = 5;
+	private static final int USERINFO = 6;
+
 	public void filter(ICommand command, IFilterContext filterContext) {
 		// TODO Auto-generated method stub
-	
+
 	}
-	
+
 	/**
 	 * This method takes an URL as input and returns it in a normalized form. There should be no change in the functionality of the URL.
-	 * This function is not speed optimized yet, as safety is more important than speed.
 	 * @param location the unnormalized URL
 	 * @return the normalized URL
 	 * @author Roland Ramthun
 	 */
 	public static String normalizeLocation(String location) {
-		
-		System.out.println("Started normalization of URL: '" + location + "'");
-		
-		String newloc = location;
-		
+
 		/*
-		 * Description: Add trailing slash to URL
+		 * What will we do and how?
 		 * 
-		 * Assumptions:
-		 * 1. A directory has to be denoted with a trailing slash (e.g. "http://paxle.net/pictures/")
-		 * 2. Every URL has to end with a slash, if it only consists of a hostname (this is in fact a directory on a server).
-		 * 3. If the string contains no single slash ("/"), there must be added the trailing slash.
+		 * First of all we create a standard Java URL object, which automatically checks the URL for correct syntax.
+		 * Then we put the key parts of this URL object in a Hashmap to work on them later.
 		 * 
-		 * Requirements:
-		 * 1. This step doesn't require any prior work on the string
+		 * Every URL has to end with a slash, if it only consists of a scheme and authority.
+		 * 
+		 * This slash is a part of the path. Even a simple URL like "http://example.org/" is a mapping on a directory on the server.
+		 * As directories have to end with a slash, there _must_ be a slash if there is no path given ending with a filename.
+		 * 
+		 * In the next step we will remove default ports from the URL.
+		 * 
+		 * Then we convert the protocol identifier and the (sub-) domain to lowercase.
+		 * Case is not important for these parts, for the path it is.
+		 * 
+		 * Then we resolve backpaths in the path.
+		 * 
+		 * As last step we reorder GET parameters alphabetically.
+		 * 
+		 * Then the resulting normalized URL is assembled, along this way possible fragments are removed, as they are simply not added
+		 * 
+		 */
+
+		//We can issue this warning, as the HTML processing parts should work on the UTF-8 string and must therefore transform the URL for themselves.
+		//So how could an unresolved encoding get here?
+		if (location.contains("%")) {
+			System.err.println("URL '" + location + "' contains encoded reserved characters, which should not be. Please file a bug-report.");
+		}
+
+		URL locurl = null;
+		try {
+			locurl = new URL(location);
+		} catch (MalformedURLException e) {
+			System.err.println("URL '" + location + "' is malformed.");
+			return null;
+		}
+
+		HashMap<Integer, String> url = new HashMap<Integer, String>();
+
+		//user:pw
+		url.put(USERINFO, locurl.getUserInfo());
+		//domain incl. subdomain
+		url.put(HOST, locurl.getHost());
+		//all parameters after the ?
+		url.put(QUERY, locurl.getQuery());
+		//the path and the document, e.g. "/path/x.htm"
+		url.put(PATH, locurl.getPath());
+		//returns the protocol without trailing ://
+		url.put(PROTOCOL, locurl.getProtocol());
+		//the given port, -1 if none given
+		url.put(PORT, "" + locurl.getPort());
+
+		// Java always strips the slash from the host, so we have to add it to the path in every case
+		if (url.get(PATH) == null) {
+			url.put(PATH, "/");
+		}
+
+		/*
+		 * Empty parameter list sets the query to "". We want it to be null, to remove the question mark from the URL while final assembly.
 		 * 
 		 * Example:
-		 * "http://paxle.net"-->"http://paxle.net/"
+		 * "http://example.org/x.php?" --> "http://example.org/x.php"
 		 */
 		
-		if (!(location.lastIndexOf("/") > location.lastIndexOf("//") + 1)) {
-			System.out.print("'" + newloc + "'" + " --> '");
-			newloc += "/";
-			System.out.println(newloc + "'");
+		if (url.get(QUERY) != null && url.get(QUERY).equals("")) {
+			url.put(QUERY, null);
 		}
 		
 		/*
-		 * Description: Remove unnecessary ports from URL
-		 * These are:
-		 * 80 for HTTP
-		 * 443 for HTTPS
-		 * 21 for FTP
-		 * 22 for Telnet
-		 * 23 for SSH
-		 * 
-		 * Assumptions: 
-		 * 1. The port is the first number in the string
-		 * 2. The port has a leading substring ":"
-		 * 3. The port is followed by a slash
-		 * 4. "Add trailing slash" has to be done before this step, as assumption 3 would be wrong otherwise
-		 * 
-		 *  Example:
-		 *  "http://paxle.net:80/download.htm" --> "http://paxle.net/download.htm"
+		 * Remove unnecessary default ports from URL
 		 */
-		
-		//TO-DO: Check port range and check for trailing slash
-		//newloc = newloc.replaceFirst(":[0-9]{1,5}", "");
-		
+		if ((url.get(PORT).equals(String.valueOf(locurl.getDefaultPort()))) || url.get(PORT).equals("-1")) {
+			url.put(PORT, "");
+		} else {
+			url.put(PORT, ":" + url.get(PORT));
+		}
+
 		/*
 		 * Convert scheme and hostname to lowercase
 		 */
-		
+		url.put(PROTOCOL, url.get(PROTOCOL).toLowerCase());
+		url.put(HOST, url.get(HOST).toLowerCase());
+
 		/*
-		 * Capitalize letters in escape sequences
-		 */
-		
-		/*
-		 * Description: Resolve backpaths
+		 * Resolve backpaths
 		 * 
 		 *  Example:
 		 *  "http://example.org/test/.././x.html" --> "http://example.org/x.html"
 		 *  "http://example.org/test/.././x/../" --> "http://example.org/"
 		 */
-		
-		//WARNING: Breaks normalization. See #29
-		//newloc = resolveBackpath(newloc);
-		
-		/*
-		 * Reorder GET-parameters alphabetically: http://paxle.net/content?lang=en&article=URL --> http://www.paxle.net/content?article=URL&lang=en
-		 */
-		
-		/*
-		 * Remove the fragment.
-		 */
-		
-		System.out.println("Finished normalization of URL: '" + location + "' --> '" + newloc + "'");
-		
-		return newloc;
-	}
-	
-    private static String resolveBackpath(String path) {
-    	
-    	final Pattern PATH_PATTERN = Pattern.compile("(/[^/]+(?<!/\\.{1,2})/)[.]{2}(?=/|$)|/\\.(?=/)|/(?=/)");
-    	
-    	if (path == null || path.length() == 0) return "/";
-        if (path.length() == 0 || path.charAt(0) != '/') { path = "/" + path; }
 
-        Matcher matcher = PATH_PATTERN.matcher(path);
-        while (matcher.find()) {
-            path = matcher.replaceAll("");
-            matcher.reset(path);
-        }
-        
-        return path.equals("")?"/":path;
-    }
+		url.put(PATH, resolveBackpath(url.get(PATH)));
+
+		//Final URL assembly:
+		// protocol + :// + userinfo + host + port + path + query
+		StringBuffer newloc = new StringBuffer(20);
+		newloc.append(url.get(PROTOCOL));
+		newloc.append("://");
+		if (url.get(USERINFO) != null) {
+			newloc.append(url.get(USERINFO) + "@");
+		}
+		newloc.append(url.get(HOST));
+		newloc.append(url.get(PORT));
+		newloc.append(url.get(PATH));
+		if (url.get(QUERY) != null) {
+			newloc.append("?" + url.get(QUERY));
+		}
+
+		System.out.println("Normalization of URL: '" + location + "' --> '" + newloc.toString() + "'");
+
+		return newloc.toString();
+	}
+
+	/**
+	 * Resolves backpaths
+	 * @param path The path of an URL
+	 * @return The path without backpath directives
+	 */
+	private static String resolveBackpath(String path) {
+
+		final Pattern PATH_PATTERN = Pattern.compile("(/[^/]+(?<!/\\.{1,2})/)[.]{2}(?=/|$)|/\\.(?=/)|/(?=/)");
+
+		if (path == null || path.length() == 0) return "/";
+		if (path.length() == 0 || path.charAt(0) != '/') { path = "/" + path; }
+
+		Matcher matcher = PATH_PATTERN.matcher(path);
+		while (matcher.find()) {
+			path = matcher.replaceAll("");
+			matcher.reset(path);
+		}
+
+		return path.equals("")?"/":path;
+	}
 
 }
--- bundles/Core/src/test/java/org/paxle/core/filter/impl/ReferenceNormalisationFilterTest.java
+++ bundles/Core/src/test/java/org/paxle/core/filter/impl/ReferenceNormalisationFilterTest.java
@@ -10,22 +10,30 @@ public class ReferenceNormalisationFilterTest extends TestCase {
 	private static final String[][] testCases = new String[][] {
 		//don't touch "good" URLs
 		{"http://www.example.org/","http://www.example.org/"},
+		{"http://www.example.org/test/test.htm","http://www.example.org/test/test.htm"},
+		//remove the default port
+		{"http://www.example.org:80","http://www.example.org/"},
+		{"https://www.example.org:443/./","https://www.example.org/"},
+		//remove empty parameters
+		{"https://www.example.org/x.php?","https://www.example.org/x.php"},
 		//add the trailing slash
 		{"http://www.example.org","http://www.example.org/"},
-		//resolve backpaths, see #29
-		//{"http://example.org/test/.././x/y.htm","http://example.org/x/y.htm"},
-		//{"http://example.org/test/../x.htm","http://example.org/x.htm"},
-		//{"http://example.org/test/.././x/.././","http://example.org/"},
+		{"http://www.example.org/test","http://www.example.org/test"},
+		//resolve backpaths
+		{"http://example.org/test/.././x/y.htm","http://example.org/x/y.htm"},
+		{"http://example.org/test/../x.htm","http://example.org/x.htm"},
+		{"http://example.org/test/.././x/.././","http://example.org/"},
+		//some combined cases
+		{"http://user:pw@www.eXamplE.orG:359/path/../path/doc.htM?k=v&k2=v2#fragment","http://user:pw@www.example.org:359/path/doc.htM?k=v&k2=v2"},
 	};
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
-	
 
-	public void testDetectCharsets() throws Exception {
 
+	public void testReferenceNormalisationFilter() throws Exception {
 		int x = 0;
 		while (x < testCases.length) {
 			String normalizationResult = ReferenceNormalizationFilter.normalizeLocation(testCases[x][0]);
