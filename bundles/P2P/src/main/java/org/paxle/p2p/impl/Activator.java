0a815900829f1202a1a2bfeb3eb3a12bd7cc1aad
Martin Thelian <martin.thelian@gmail.com>
2008-02-26 06:53:51 +0000

    *) next step to mavenize paxle (more testing needed for this bundle)
    git-svn-id: svn://svn.paxle.net/paxle/trunk@621 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/P2P/src/main/java/org/paxle/p2p/impl/Activator.java
+++ bundles/P2P/src/main/java/org/paxle/p2p/impl/Activator.java
@@ -5,17 +5,12 @@ package org.paxle.p2p.impl;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Properties;
 
-import meteor.lookup.MeteorPeer;
 import net.jxta.peergroup.PeerGroup;
 
-import org.apache.velocity.app.VelocityEngine;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import org.osgi.service.http.HttpService;
-import org.paxle.p2p.FirewallCheckViewServlet;
 import org.paxle.p2p.IP2PManager;
 import org.paxle.p2p.services.IService;
 import org.paxle.p2p.services.IServiceClient;
@@ -40,8 +35,6 @@ public class Activator implements BundleActivator {
 	public static BundleContext bc;				
 	
 	private static P2PManager p2pManager = null;
-	
-	private static VelocityEngine velocity = null;
 	
 	/**
 	 * This function is called by the osgi-framework to start the bundle.
@@ -95,9 +88,9 @@ public class Activator implements BundleActivator {
 			);
 		}
 		
-		MeteorPeer mp = new MeteorPeer();
-		mp.init(p2pManager.getPeerGroup(), null, null);
-		mp.startApp(null);
+//		MeteorPeer mp = new MeteorPeer();
+//		mp.init(p2pManager.getPeerGroup(), null, null);
+//		mp.startApp(null);
 
 		/* ==========================================================
 		 * Register Services
@@ -138,58 +131,5 @@ public class Activator implements BundleActivator {
 		bc = null;		
 		p2pManager.terminate();
 		p2pManager = null;
-		velocity = null;
-	}
-	
-	/**
-	 * TODO: replace this
-	 * 
-	 * To detect if a peer is capable to work as a relay peer can do the following
-	 * 1.) contact an already known relay peer
-	 * 2.) force the relay peer to open a plain tcp connection to the jxta port of our peer
-	 * If the peer is capable to accept connection from outside, the connection will be accepted, e.g.
-	 * <pre>
-	 * theli@theli:~/workspace/JxtaTest2$ telnet 192.168.0.93 9701
-	 * Trying 192.168.0.93...
-	 * Connected to 192.168.0.93.
-	 * Escape character is '^]'.
-	 * JXTAHELLO tcp://192.168.0.96:35381 tcp://192.168.0.93:9701 urn:jxta:uuid-59616261646162614E50472050325033E0BE60AC7C38466FB8EFCB6A4ED2230203 0 1.1
-	 * </pre>
-	 * 
-	 * @deprecated
-	 */
-	private void initFirewallCheckActive() {
-		new PingFirewallcheckService("http://test.laxu.de/firewallcheck.php?port=8080");
-	}
-	
-	/**
-	 * @deprecated not required anymore, see {@link #initFirewallCheckActive()}
-	 */
-	private void initFirewallCheckPassive() {
-		//getting a reference to the osgi http service
-		ServiceReference sr = bc.getServiceReference(HttpService.class.getName());
-		if(sr != null) {
-			// getting the http service
-			HttpService http = (HttpService) bc.getService(sr);
-			if(http != null) {			
-				// init velocity
-				try {
-					Properties velocityConfig = new Properties();
-					velocityConfig.load(Activator.class.getResourceAsStream("/resources/velocity.properties"));
-					velocityConfig.setProperty("jar.resource.loader.path", "jar:" + bc.getBundle().getLocation());		
-					velocity = new VelocityEngine();
-					velocity.init(velocityConfig);		
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				// registering the servlet which will be accessible using 
-				try {
-					http.registerServlet("/paxle/firewallcheck", new FirewallCheckViewServlet(p2pManager, velocity), null, null);
-				} catch (Exception e){
-					e.printStackTrace();
-				}
-			}
-		}		
-	}
+	}	
 }
