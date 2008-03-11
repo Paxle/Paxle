d3b18ce31fa44cb447fd717ffdbb270caa800625
Martin Thelian <martin.thelian@gmail.com>
2008-03-11 07:23:05 +0000

    *) moving abstract parser-test class to ParserCore so that it can be re-used by other    sub-parser-tests    - ATTENTION: please execute "mvn eclipse:clean eclipse:eclipse" to refresh your eclipse classpath
    git-svn-id: svn://svn.paxle.net/paxle/trunk@694 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/ParserCore/src/test/java/org/paxle/parser/impl/AParserTest.java
@@ -0,0 +1,39 @@
+package org.paxle.parser.impl;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.paxle.core.io.temp.ITempDir;
+import org.paxle.core.io.temp.ITempFileManager;
+import org.paxle.parser.ParserContext;
+
+
+public abstract class AParserTest extends TestCase {
+	protected ParserContext parserContext = null;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+
+		// create a parser context with a dummy temp-file-manager
+		this.parserContext = new ParserContext(null,null,null, new ITempFileManager() {		
+			public void setTempDirFor(ITempDir arg0, String... arg1) { }		
+			public void removeTempDirFor(String... arg0) { }
+
+			public void releaseTempFile(File arg0) throws FileNotFoundException, IOException {
+				if (arg0 != null) arg0.delete();
+			}
+
+			public File createTempFile() throws IOException {
+				File tempfile = File.createTempFile("parserTest", ".tmp");
+				tempfile.deleteOnExit();
+				return tempfile;
+			}
+
+		});
+		ParserContext.setCurrentContext(this.parserContext);		
+	}
+}
--- bundles/ParserMsOffice/src/test/java/org/paxle/parser/msoffice/impl/AMsOfficeParserTest.java
+++ bundles/ParserMsOffice/src/test/java/org/paxle/parser/msoffice/impl/AMsOfficeParserTest.java
@@ -1,39 +1,13 @@
 package org.paxle.parser.msoffice.impl;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.paxle.core.io.temp.ITempDir;
-import org.paxle.core.io.temp.ITempFileManager;
 import org.paxle.parser.ParserContext;
+import org.paxle.parser.impl.AParserTest;
 
-import junit.framework.TestCase;
-
-public abstract class AMsOfficeParserTest extends TestCase {
+public abstract class AMsOfficeParserTest extends AParserTest {
 	protected ParserContext parserContext = null;
 	
 	@Override
 	protected void setUp() throws Exception {
-		super.setUp();
-		
-
-		// create a parser context with a dummy temp-file-manager
-		this.parserContext = new ParserContext(null,null,null, new ITempFileManager() {		
-			public void setTempDirFor(ITempDir arg0, String... arg1) { }		
-			public void removeTempDirFor(String... arg0) { }
-
-			public void releaseTempFile(File arg0) throws FileNotFoundException, IOException {
-				if (arg0 != null) arg0.delete();
-			}
-
-			public File createTempFile() throws IOException {
-				File tempfile = File.createTempFile("mswordParser", ".doc");
-				tempfile.deleteOnExit();
-				return tempfile;
-			}
-
-		});
-		ParserContext.setCurrentContext(this.parserContext);		
+		super.setUp();	
 	}
 }
