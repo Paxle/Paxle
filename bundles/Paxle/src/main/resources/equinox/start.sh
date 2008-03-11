bc5dc9f669e47b90c6a4f91614bb01a1320a2204
Martin Thelian <martin.thelian@gmail.com>
2008-03-11 07:01:49 +0000

    *) DIJre6Backend mavenized    See: https://bugs.pxl.li/view.php?id=60    - ATTENTION: this bundle will only be compiled if you are using JRE1.6 *) aggregation-pom: build-profile added for compilation of JRE1.6 only bundles    - for platforms that have no JRE1.6 installed, compilation of those bundles      is skipped. *) start.sh: java is located using JAVA_HOME now *) Root: .project added to svn:ignore
    git-svn-id: svn://svn.paxle.net/paxle/trunk@693 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Paxle/src/main/resources/equinox/start.sh
+++ bundles/Paxle/src/main/resources/equinox/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "-rdebug" ]; then
-	nice -n 15 java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
+	nice -n 15 $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
 else
-	nice -n 15 java -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
+	nice -n 15 $JAVA_HOME/bin/java -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
 fi
