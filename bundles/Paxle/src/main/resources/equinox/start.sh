90c873de23fbae165ab1e643ec829529d33965c4
Alexander Schier <allo@laxu.de>
2008-03-12 12:28:13 +0000

    better search for java binary if JAVA_HOME is not set.
    git-svn-id: svn://svn.paxle.net/paxle/trunk@701 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Paxle/src/main/resources/equinox/start.sh
+++ bundles/Paxle/src/main/resources/equinox/start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+JAVA="`which java`"
+test -z "$JAVA" && test \! -z "$JAVA_HOME" && JAVA=$JAVA_HOME/bin/java
+
 if [ "$1" = "-rdebug" ]; then
-	nice -n 15 $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
+	nice -n 15 $JAVA -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
 else
-	nice -n 15 $JAVA_HOME/bin/java -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
+	nice -n 15 $JAVA -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -console
 fi
