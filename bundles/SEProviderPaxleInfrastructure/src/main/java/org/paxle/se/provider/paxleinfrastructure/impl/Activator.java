3c01bab5dd8618091376647b4473a9c5b2c09144
Alexander Schier <allo@laxu.de>
2008-03-12 11:59:52 +0000

    (sample) SearchEnginge Provider, answering to queries for paxle-urls
    git-svn-id: svn://svn.paxle.net/paxle/trunk@699 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/SEProviderPaxleInfrastructure/src/main/java/org/paxle/se/provider/paxleinfrastructure/impl/Activator.java
@@ -0,0 +1,34 @@
+package org.paxle.se.provider.paxleinfrastructure.impl;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.paxle.se.search.ISearchProvider;
+
+public class Activator implements BundleActivator {
+
+	/**
+	 * A reference to the {@link BundleContext bundle-context}
+	 */	
+	public static BundleContext bc = null;
+
+	/**
+	 * This function is called by the osgi-framework to start the bundle.
+	 * @see BundleActivator#start(BundleContext) 
+	 */		
+	public void start(BundleContext context) throws Exception {
+		bc = context;
+
+		ISearchProvider provider = new PaxleInfrastructureSearchProvider();
+		bc.registerService(ISearchProvider.class.getName(), provider, new Hashtable<String,String>());
+	}
+
+	/**
+	 * This function is called by the osgi-framework to stop the bundle.
+	 * @see BundleActivator#stop(BundleContext)
+	 */		
+	public void stop(BundleContext context) throws Exception {
+		bc = null;
+	}
+}
