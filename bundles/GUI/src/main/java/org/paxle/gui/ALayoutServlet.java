70d8150a046f030dadafc2a994bc222562c96ee8
herrlich <herrlich@paxle.net>
2008-03-14 11:48:23 +0000

    changed to abstract class, added abstract method handleRequest
    git-svn-id: svn://svn.paxle.net/paxle/trunk@740 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/GUI/src/main/java/org/paxle/gui/ALayoutServlet.java
+++ bundles/GUI/src/main/java/org/paxle/gui/ALayoutServlet.java
@@ -3,20 +3,24 @@ package org.paxle.gui;
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.collections.ExtendedProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.Template;
+import org.apache.velocity.context.Context;
 import org.apache.velocity.tools.view.servlet.VelocityLayoutServlet;
 
-public class ALayoutServlet extends VelocityLayoutServlet {	
-    private static final long serialVersionUID = 1L;    
+public abstract class ALayoutServlet extends VelocityLayoutServlet {
+    private static final long serialVersionUID = 1L;
     public static final String SERVICE_MANAGER = "manager";
 	
     /**
      * Logger
      */
-    protected Log logger = LogFactory.getLog(this.getClass());    
+    protected Log logger = LogFactory.getLog(this.getClass());
     
     /**
      * The location of the OSGi bundle this servlet belongs to.
@@ -42,11 +46,13 @@ public class ALayoutServlet extends VelocityLayoutServlet {
 	protected ExtendedProperties loadConfiguration(ServletConfig config) throws IOException {
 		ExtendedProperties props = super.loadConfiguration(config);
 		
-		if (this.bundleLocation != null) {			
+		if (this.bundleLocation != null) {
 			props.addProperty("jar.resource.loader.path", "jar:" + this.bundleLocation + ",jar:" + config.getInitParameter("bundle.location"));
 		} else if (config.getInitParameter("bundle.location") != null) {
 			props.addProperty("jar.resource.loader.path", "jar:" + config.getInitParameter("bundle.location"));
-		} 
+		}
 		return props;
 	}
+	
+	public abstract Template handleRequest( HttpServletRequest request, HttpServletResponse response, Context context);
 }
