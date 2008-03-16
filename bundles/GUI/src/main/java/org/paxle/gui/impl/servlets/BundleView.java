5fcb5a7269ed656650745991f6f79293ba0e4dba
Martin Thelian <martin.thelian@gmail.com>
2008-03-16 12:09:48 +0000

    *) bundles can now also be uploaded via the browser *) better errorhandling *) bundle-last-modified is displayed as date now
    git-svn-id: svn://svn.paxle.net/paxle/trunk@788 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/GUI/src/main/java/org/paxle/gui/impl/servlets/BundleView.java
+++ bundles/GUI/src/main/java/org/paxle/gui/impl/servlets/BundleView.java
@@ -1,16 +1,24 @@
 package org.paxle.gui.impl.servlets;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.context.Context;
-
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
 import org.paxle.gui.ALayoutServlet;
 import org.paxle.gui.impl.ServiceManager;
 
@@ -41,31 +49,109 @@ public class BundleView extends ALayoutServlet {
             template = this.getTemplate("/resources/templates/BundleView.vm");
             
             ServiceManager manager = (ServiceManager) context.get(SERVICE_MANAGER);
-            if (request.getParameter("update") != null) {
-                Bundle bundle = manager.getBundle(Long.parseLong(request.getParameter("bundleID")));
-                bundle.update();
-            } else if (request.getParameter("start") != null) {
-                Bundle bundle = manager.getBundle(Long.parseLong(request.getParameter("bundleID")));
-                bundle.start();
-            } else if (request.getParameter("stop") != null) {
-                Bundle bundle = manager.getBundle(Long.parseLong(request.getParameter("bundleID")));
-                bundle.stop();
-            } else if (request.getParameter("restart") != null) {
-                Bundle bundle = manager.getBundle(Long.parseLong(request.getParameter("bundleID")));
-                bundle.stop();
-                bundle.start();
-            }else if(request.getParameter("uninstall") != null){
-            	Bundle bundle = manager.getBundle(Long.parseLong(request.getParameter("bundleID")));
-                bundle.uninstall();
-            }else if(request.getParameter("install") != null && request.getParameter("bundlePath")!=null){
+            if (request.getParameter("bundleID") != null) {
+            	Long bundleID = Long.parseLong(request.getParameter("bundleID"));
+            	Bundle bundle = manager.getBundle(bundleID);
+        		if (bundle == null) {
+        			String errorMsg = String.format(
+        					"Bundle with ID '%s' not found.", 
+        					request.getParameter("bundleID")
+        			);
+        			this.logger.warn(errorMsg);
+        			context.put("errorMsg",errorMsg);
+        		} else {
+        			try {
+        				if (request.getParameter("update") != null) {
+        					bundle.update();
+        				} else if (request.getParameter("start") != null) {
+        					bundle.start();
+        				} else if (request.getParameter("stop") != null) {
+        					bundle.stop();
+        				} else if (request.getParameter("restart") != null) {
+        					bundle.stop();
+        					bundle.start();
+        				}else if(request.getParameter("uninstall") != null){
+        					bundle.uninstall();
+        				}
+        			} catch (BundleException e) {
+            			String errorMsg = String.format(
+            					"Unexpected exception while operating on bundle with ID '%s", 
+            					request.getParameter("bundleID")
+            			);
+            			this.logger.warn(errorMsg, e);
+            			context.put("errorMsg",e.getMessage());
+        			}
+        		}
+            }else if(request.getParameter("installURL") != null && request.getParameter("bundlePath")!=null){
             	ServiceManager.context.installBundle(request.getParameter("bundlePath"));
             } else if (request.getParameter("details") != null) {
             	Bundle bundle = manager.getBundle(Long.parseLong(request.getParameter("bundleID")));
             	context.put("bundle", bundle);
-            }       
+            } else if (ServletFileUpload.isMultipartContent(request)) {
+
+            	// Create a factory for disk-based file items
+            	FileItemFactory factory = new DiskFileItemFactory();
+
+//          	// Set factory constraints
+//          	factory.setSizeThreshold(yourMaxMemorySize);
+//          	factory.setRepository(yourTempDirectory);
+
+            	// Create a new file upload handler
+            	ServletFileUpload upload = new ServletFileUpload(factory);
+
+//          	// Set overall request size constraint
+//          	upload.setSizeMax(yourMaxRequestSize);
+
+            	// Parse the request
+            	List<FileItem> items = upload.parseRequest(request);
+
+            	// Process the uploaded items
+            	Iterator<FileItem> iter = items.iterator();
+            	while (iter.hasNext()) {
+            		FileItem item = iter.next();
+
+            		if (!item.isFormField()) {
+            			if (!item.getFieldName().equals("bundleJar")) {
+            				String errorMsg = String.format("Unknown file-upload field '%s'.",item.getFieldName());
+            				this.logger.warn(errorMsg);
+            				context.put("errorMsg",errorMsg);
+            				continue;
+            			}
+            			
+            		    String fileName = item.getName();
+            		    if (fileName != null) {
+            		    	fileName = FilenameUtils.getName(fileName);
+            		    } else {
+            		    	String errorMsg = String.format("Fileupload field '%s' has no valid filename.", item.getFieldName());
+            		    	this.logger.warn(errorMsg);
+            		    	context.put("errorMsg",errorMsg);
+            		    	continue;
+            		    }
+            			
+            			// write the bundle to disk
+            		    File targetFile = new File(fileName);
+            		    if (targetFile.exists()) {
+            		    	String errorMsg = String.format("Targetfile '%s' already exists.",targetFile.getCanonicalFile().toString());
+            		    	this.logger.warn(errorMsg);
+            		    	context.put("errorMsg",errorMsg);
+            		    	continue;
+            		    }            		    
+            		    item.write(targetFile);
+            		    
+            		    // installing bundle
+            			ServiceManager.context.installBundle(targetFile.toURL().toString());
+
+            			// cleanup
+            			item.delete();
+            		}
+            	}
+
+            }
+            
+            
             context.put("bundles", bundles2map(manager.getBundles()));
             context.put("states", states);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             System.err.println("Exception caught: " + e.getMessage());
             e.printStackTrace();
         }
