c00908fc7c9a1f5a534b21868bb67473da98d675
Matthias Söhnholz <miniminko@googlemail.com>
2008-03-19 10:56:15 +0000

    *) bugfix for SearchProviederManager
    git-svn-id: svn://svn.paxle.net/paxle/trunk@844 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/SearchEngine/src/main/java/org/paxle/se/search/impl/SearchProviderManager.java
+++ bundles/SearchEngine/src/main/java/org/paxle/se/search/impl/SearchProviderManager.java
@@ -2,7 +2,6 @@
 package org.paxle.se.search.impl;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
@@ -31,34 +30,34 @@ import org.paxle.se.search.SearchException;
 
 public class SearchProviderManager implements ISearchProviderManager {
 	private static final String DISABLED_PROVIDERS = ISearchProviderManager.class.getName() + "." + "disabledProviders";	
-	
+
 	private final ExecutorService execService;
 	private final LinkedHashMap<Long,ISearchProvider> providers = new LinkedHashMap<Long, ISearchProvider>();
-	
+
 	/**
 	 * for logging
 	 */
 	private final Log logger = LogFactory.getLog(SearchProviderManager.class);
-	
+
 	/**
 	 * The names of all currently disabled providers
 	 */
 	private Set<String> disabledProviders = new HashSet<String>();
-	
+
 	/**
 	 * The properties of this component
 	 */
 	private Properties props = null;
-	
+
 	public SearchProviderManager(Properties props) {
 		this.execService = Executors.newCachedThreadPool();
-		
+
 		this.props = props;
 		if (this.props != null && this.props.containsKey(DISABLED_PROVIDERS)) {
 			this.disabledProviders = this.props.getSet(DISABLED_PROVIDERS);
 		}		
 	}
-	
+
 	synchronized void addProvider(Long providerID, ISearchProvider provider) {
 		if (providerID == null) throw new NullPointerException("The provider-ID is null");
 		if (provider == null) throw new NullPointerException("The provider is null");
@@ -66,11 +65,11 @@ public class SearchProviderManager implements ISearchProviderManager {
 			String format = String.format("Unable to register se-provider with ID '%s'. Provider already registered.", providerID.toString());
 			this.logger.error(format);
 		}
-		
+
 		this.logger.info("added search provider: " + provider.getClass().getName());
 		this.providers.put(providerID, provider);
 	}
-	
+
 	synchronized void removeProvider(Long providerID) {
 		if (providerID == null) throw new NullPointerException("The provider-ID is null");
 		if (!this.providers.containsKey(providerID)) {
@@ -78,19 +77,19 @@ public class SearchProviderManager implements ISearchProviderManager {
 			this.logger.error(format);
 			return;
 		}
-		
+
 		final ISearchProvider provider = this.providers.remove(providerID);
 		this.logger.info("removed search provider: " + provider.getClass().getName());
 		// this.pqp.removeTokenFactory(number);
 	}
-	
+
 	/**
 	 * @see ISearchProviderManager#getSearchProviders()
 	 */
 	public Collection<ISearchProvider> getSearchProviders() {
 		return Collections.unmodifiableCollection(this.providers.values());
 	}
-	
+
 	public void shutdown() throws IOException {
 		this.logger.info("search provider manager is shutting down...");
 		this.logger.debug("waiting for searches to finish");
@@ -100,7 +99,7 @@ public class SearchProviderManager implements ISearchProviderManager {
 		// this.pqp.clearTokenFactories();
 		this.logger.info("shutdown complete");
 	}
-	
+
 	public List<ISearchResult> search(String paxleQuery,
 			int maxResults,
 			long timeout) throws InterruptedException, ExecutionException, SearchException {
@@ -108,48 +107,48 @@ public class SearchProviderManager implements ISearchProviderManager {
 		search(paxleQuery, maxResults, timeout, collector);
 		return collector;
 	}
-	
+
 	public void search(String paxleQuery,
 			int maxResults,
 			long timeout,
 			ISearchResultCollector results) throws InterruptedException, ExecutionException, SearchException {
 		final CompletionService<ISearchResult> execCompletionService = new ExecutorCompletionService<ISearchResult>(this.execService);
-		
+
 		// final List<AToken> queries = this.pqp.parse(paxleQuery);
 		final AToken query = PaxleQueryParser.parse(paxleQuery);
 		if (query == null)
 			throw new SearchException(paxleQuery, "Illegal query (maybe too short?)");
-		
+
 		int n = providers.size();
-		
+
 		int usedProviders = 0;
-		for (int i=0; i<n; i++) {
-			ISearchProvider provider = this.providers.get(i);
-			String providerClassName = provider.getClass().getName();
-			if (disabledProviders.contains(providerClassName)) {
-				this.logger.debug(String.format("Skipping disabled provider %s ...", providerClassName));
-				continue;
+		if (n > 0) {
+			for(ISearchProvider provider : this.providers.values()) {
+				String providerClassName = provider.getClass().getName();
+				if (disabledProviders.contains(providerClassName)) {
+					this.logger.debug(String.format("Skipping disabled provider %s ...", providerClassName));
+					continue;
+				}
+
+				// final ISearchRequest searchRequest = new SearchRequest(queries.get(i), maxResults, timeout);
+				final ISearchRequest searchRequest = new SearchRequest(query, maxResults, timeout);
+				execCompletionService.submit(new SearchProviderCallable(provider, searchRequest));
+
+				usedProviders++;
 			}
-			
-			// final ISearchRequest searchRequest = new SearchRequest(queries.get(i), maxResults, timeout);
-			final ISearchRequest searchRequest = new SearchRequest(query, maxResults, timeout);
-			execCompletionService.submit(new SearchProviderCallable(provider, searchRequest));
-			
-			usedProviders++;
 		}
-		
 		for (int i = 0; i < usedProviders; ++i) {
 			final long start = System.currentTimeMillis();
-			
+
 			final Future<ISearchResult> future = execCompletionService.poll(timeout, TimeUnit.MILLISECONDS);
 			if (future != null) {
 				final ISearchResult r = future.get();
-				
+
 				if (r != null) {
 					results.collect(r);
 				}
 			}
-			
+
 			final long diff = System.currentTimeMillis() - start;
 			if ((timeout-=diff)<= 0) break;
 		}
