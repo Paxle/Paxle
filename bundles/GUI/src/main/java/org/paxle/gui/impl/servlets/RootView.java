cb6c12027f74272c1e133dc44e9eded26eb893cc
Martin Thelian <martin.thelian@gmail.com>
2008-04-29 06:06:04 +0000

    *) Suppress waring if template is null |> 29 Apr 2008 07:01:17 WARN  org.apache.velocity  - VelocityViewServlet: couldn't find template to match request.
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1060 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/GUI/src/main/java/org/paxle/gui/impl/servlets/RootView.java
+++ bundles/GUI/src/main/java/org/paxle/gui/impl/servlets/RootView.java
@@ -2,6 +2,7 @@ package org.paxle.gui.impl.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,19 +14,18 @@ public class RootView extends ALayoutServlet {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// just a redirection to the search view
+		response.sendRedirect("/search");
+	}
+	
     public Template handleRequest( 
     		HttpServletRequest request,
             HttpServletResponse response,
             Context context 
     ) {
-
-    	// just a redirection to the search view
-        try {
-			response.sendRedirect("/search");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	// nothing todo here
         return null;
     }
 
