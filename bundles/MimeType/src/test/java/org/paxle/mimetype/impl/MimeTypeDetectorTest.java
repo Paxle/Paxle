7ca7c5d1aa4989df1f452b9819a56c9da5871eb9
Franz Brauße <dev@karlchenofhell.org>
2008-05-05 00:11:51 +0000

    - added test-cases for LHA-, Tar- and 7zip-archives as well as a library found on the net which erroneusly has been reported to be of type application/x-lha (fixed)
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1075 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/MimeType/src/test/java/org/paxle/mimetype/impl/MimeTypeDetectorTest.java
+++ bundles/MimeType/src/test/java/org/paxle/mimetype/impl/MimeTypeDetectorTest.java
@@ -7,12 +7,12 @@ import junit.framework.TestCase;
 public class MimeTypeDetectorTest extends TestCase {
 	private static final int TESTFILE_NAME = 0;
 	private static final int TESTFILE_MIMETYPE = 1;
-
+	
 	/**
 	 * Mimetype detector class
 	 */
 	private MimeTypeDetector detector;
-
+	
 	/**
 	 * Testfiles to test
 	 */
@@ -22,30 +22,39 @@ public class MimeTypeDetectorTest extends TestCase {
 		{"test.pdf","application/pdf"},
 		{"test.zip","application/zip"},
 		{"test.bz2","application/x-bzip2"},
-		{"test.gz","application/x-gzip"}
+		{"test.gz","application/x-gzip"},
+		{"test.7z","application/x-7z-compressed"},
+		{"test.lha","application/x-lha"},
+		{"test.tar","application/x-tar"},
+		{"rot3d.library",null},
 	};
-
+	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		this.detector = new MimeTypeDetector(null);
 	}
-
+	
 	public void testDetectHtml() throws Exception {
-
+		
 		for (String[] testCase : testCases) {
 			File testFile = new File("src/test/resources/" + testCase[TESTFILE_NAME]);
 			assertTrue(testFile.exists());
-
+			
 			String mimeType = this.detector.getMimeType(testFile);
-
-			assertNotNull(mimeType);
-			assertEquals(testCase[TESTFILE_MIMETYPE], mimeType);
+			
 			System.out.println(String.format(
 					"%s has mimetype %s.",
 					testCase[TESTFILE_NAME],
 					mimeType
 			));
+			final String expected = testCase[TESTFILE_MIMETYPE];
+			if (expected == null) {
+				assertNull("at " + testFile, mimeType);
+			} else {
+				assertNotNull("at " + testFile, mimeType);
+				assertEquals(expected, mimeType);
+			}
 		}
 	}
 }
