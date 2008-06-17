e5d4d0b303d94c66b3ee8ebca56c55a9ceb4b390
Martin Thelian <martin.thelian@gmail.com>
2008-06-17 05:20:56 +0000

    *) using a buffered-stream so that subparsers inherited from ASubparser can use mark/reset on the inputstream
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1145 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/ParserCore/src/main/java/org/paxle/parser/ASubParser.java
+++ bundles/ParserCore/src/main/java/org/paxle/parser/ASubParser.java
@@ -1,6 +1,7 @@
 
 package org.paxle.parser;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,29 +17,30 @@ import org.paxle.core.io.temp.ITempFileManager;
 
 public abstract class ASubParser implements ISubParser {
 	
-	public IParserDocument parse(URI location, String charset, File content)
-			throws ParserException, UnsupportedEncodingException, IOException {
-		final FileInputStream fis = new FileInputStream(content);
+	public IParserDocument parse(URI location, String charset, File content) throws ParserException, UnsupportedEncodingException, IOException {
+		InputStream in = null;
 		try {
-			return parse(location, charset, fis);
-		} finally { fis.close(); }
+			in = new BufferedInputStream(new FileInputStream(content));			
+			return parse(location, charset, in);
+		} finally {
+			if (in != null) try { in.close(); } catch (Exception e) {/* ignore this */} 
+		}
 	}
 	
-	public IParserDocument parse(URI location, String charset, InputStream is)
-			throws ParserException, UnsupportedEncodingException, IOException {
+	public IParserDocument parse(URI location, String charset, InputStream is) throws ParserException, UnsupportedEncodingException, IOException {
 		final ParserContext context = ParserContext.getCurrentContext();
-		if (context == null)
-			throw new ParserException("cannot access ParserContext whereas this method must be used from within a sub-parser");
+		if (context == null) throw new ParserException("cannot access ParserContext whereas this method must be used from within a sub-parser");
 		
 		final ITempFileManager tfm = context.getTempFileManager();
-		if (tfm == null)
-			throw new ParserException("cannot access temp-file manager");
+		if (tfm == null) throw new ParserException("cannot access temp-file manager");
 		
 		final File content = tfm.createTempFile();
 		final BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(content));
 		try {
 			IOTools.copy(is, bos);
-		} finally { bos.close(); }
+		} finally { 
+			bos.close(); 
+		}
 		
 		return parse(location, charset, content);
 	}
