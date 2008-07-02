759f654d183937a9f9e9cf2050bcc73c72bb389d
Martin Thelian <martin.thelian@gmail.com>
2008-07-02 19:05:06 +0000

    *) bugfix in urlextractor-filter - links with not-ok status were not skipped
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1192 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/DataLayer/src/main/java/org/paxle/data/db/impl/UrlExtractorFilter.java
+++ bundles/DataLayer/src/main/java/org/paxle/data/db/impl/UrlExtractorFilter.java
@@ -4,19 +4,20 @@ package org.paxle.data.db.impl;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.paxle.core.doc.IParserDocument;
 import org.paxle.core.doc.LinkInfo;
+import org.paxle.core.doc.LinkInfo.Status;
 import org.paxle.core.filter.IFilter;
 import org.paxle.core.filter.IFilterContext;
 import org.paxle.core.queue.ICommand;
 
 public class UrlExtractorFilter implements IFilter<ICommand> {
 	
-	private static class Counter {
-		
+	private static class Counter {		
 		public int c = 0;
 		public int known = 0;
 	}
@@ -40,7 +41,8 @@ public class UrlExtractorFilter implements IFilter<ICommand> {
 		// getting the link map
 		final Counter c = new Counter();
 		this.extractLinks(command, parserDoc, c);
-		logger.info(String.format("Extracted %d new and %d already known URIs from '%s'",
+		logger.info(String.format(
+				"Extracted %d new and %d already known URIs from '%s'",
 				Integer.valueOf(c.c - c.known), Integer.valueOf(c.known), command.getLocation()));
 	}
 	
@@ -62,17 +64,29 @@ public class UrlExtractorFilter implements IFilter<ICommand> {
 	}
 	
 	private void extractLinks(final ICommand command, Map<URI, LinkInfo> linkMap, final Counter c) {
-		ArrayList<URI> locations = new ArrayList<URI>();
+		ArrayList<URI> validLocations = new ArrayList<URI>();
 		
-		for (URI ref : linkMap.keySet()) {
+		for (Entry<URI, LinkInfo> link : linkMap.entrySet()) {
+			URI ref = link.getKey();
+			LinkInfo meta = link.getValue();
+			
+			// check if the URI exceeds max length
 			if (ref.toString().length() > 512) {
 				this.logger.debug("Skipping too long URL: " + ref);
 				continue;
+			} else if (!meta.hasStatus(Status.OK)) {
+				this.logger.debug(String.format(
+						"Skipping URL because of status '%s' (%s): %s",
+						meta.getStatus(),
+						meta.getStatusText(),
+						ref
+				));
 			}
 			
 			// add command into list
-			locations.add(ref);
+			validLocations.add(ref);
 		}
+
 
 		// store commands into DB
 		if (!db.isClosed()) {
@@ -80,9 +94,9 @@ public class UrlExtractorFilter implements IFilter<ICommand> {
 			c.known += db.storeUnknownLocations(
 					command.getProfileOID(),
 					command.getDepth()+1,
-					locations
+					validLocations
 			);
-			c.c += locations.size();
+			c.c += validLocations.size();
 		} else {
 			this.logger.error(String.format(
 					"Unable to write linkmap of location '%s' to db. Database already closed.",
