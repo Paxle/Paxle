78cf0ecf30522de203c6ea9610024ece14eb2680
Martin Thelian <martin.thelian@gmail.com>
2008-07-04 05:58:22 +0000

    *) removing static reference to bundle context
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1201 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/MimeType/src/main/java/org/paxle/mimetype/impl/Activator.java
+++ bundles/MimeType/src/main/java/org/paxle/mimetype/impl/Activator.java
@@ -5,19 +5,12 @@ import org.osgi.framework.BundleContext;
 import org.paxle.core.mimetype.IMimeTypeDetector;
 
 public class Activator implements BundleActivator {
-
-	/**
-	 * A reference to the {@link BundleContext bundle-context}
-	 */
-	public static BundleContext bc;			
 	
 	/**
 	 * This function is called by the osgi-framework to start the bundle.
 	 * @see BundleActivator#start(BundleContext) 
 	 */		
-	public void start(BundleContext context) throws Exception {
-		bc = context;
-		
+	public void start(BundleContext bc) throws Exception {
 		Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
 		
 		/* ==========================================================
@@ -38,7 +31,6 @@ public class Activator implements BundleActivator {
 	 * @see BundleActivator#stop(BundleContext)
 	 */		
 	public void stop(BundleContext context) throws Exception {
-		// cleanup
-		bc = null;		
+		// nothing todo here
 	}
 }
