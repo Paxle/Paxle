42d11ca48eeffd52239fbd8fde73ca0bb40da3fe
Martin Thelian <martin.thelian@gmail.com>
2008-08-17 13:14:16 +0000

    *) name of the equinox-runtime-jar file is now configured via property - this allows us to remove all hardcoded names from startup and shortcut files
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1230 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Paxle/src/main/resources/equinox/start.sh
+++ bundles/Paxle/src/main/resources/equinox/start.sh
@@ -4,7 +4,7 @@ test \! -z "$JAVA_HOME" && JAVA=$JAVA_HOME/bin/java
 test -z "$JAVA" && JAVA="`which java`"
 
 if [ "$1" = "-rdebug" ]; then
-	nice -n 15 $JAVA -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -refresh -console
+	nice -n 15 $JAVA -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar ${equinox.runtime.jar} -refresh -console
 else
-	nice -n 15 $JAVA -jar org.eclipse.osgi_3.3.1.R33x_v20070828.jar -refresh -console
+	nice -n 15 $JAVA -jar ${equinox.runtime.jar} -refresh -console
 fi
