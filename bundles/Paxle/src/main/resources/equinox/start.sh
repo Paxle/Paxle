279f9873794bed1e7b45a29e6744bdd30afef755
Martin Thelian <martin.thelian@gmail.com>
2008-08-18 06:23:57 +0000

    *) start.sh now also reads java runtime settings (e.g. memory settings) from start.ini
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1235 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Paxle/src/main/resources/equinox/start.sh
+++ bundles/Paxle/src/main/resources/equinox/start.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
+# java runtime lookup
 test \! -z "$JAVA_HOME" && JAVA=$JAVA_HOME/bin/java
 test -z "$JAVA" && JAVA="`which java`"
 
+# change directory
+cd "`dirname $0`"
+
+# read java runtime options
+JAVA_ARGS=""
+if [ -f start.ini ]
+then
+	JAVA_ARGS_FILE=`cat start.ini`
+	for JAVA_ARG in $JAVA_ARGS_FILE
+	do		
+		JAVA_ARG=`echo $JAVA_ARG | sed 's/\r//g'`
+		if [ -n $JAVA_ARG ]; then JAVA_ARGS="$JAVA_ARG $JAVA_ARGS"; fi;
+	done
+	echo "Using Java Args: $JAVA_ARGS"
+fi
+
+# startup java
 if [ "$1" = "-rdebug" ]; then
-	nice -n 15 $JAVA -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar ${equinox.runtime.jar} -refresh -console
+	nice -n 15 $JAVA $JAVA_ARGS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar ${equinox.runtime.jar} -refresh -console
 else
-	nice -n 15 $JAVA -jar ${equinox.runtime.jar} -refresh -console
+	nice -n 15 $JAVA $JAVA_ARGS -jar ${equinox.runtime.jar} -refresh -console
 fi
