a0c097c6c753e0eb4f7885d42e7774cccea763a9
Martin Thelian <martin.thelian@gmail.com>
2008-08-28 18:46:09 +0000

    *) logging-view now also displays log4j messages *) some icons updated
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1254 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/GUI/src/main/java/org/paxle/gui/impl/Log4jMemoryAppender.java
@@ -0,0 +1,96 @@
+package org.paxle.gui.impl;
+
+import java.util.Iterator;
+
+import org.apache.commons.collections.Buffer;
+import org.apache.commons.collections.BufferUtils;
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogEntry;
+import org.osgi.service.log.LogService;
+
+public class Log4jMemoryAppender extends AppenderSkeleton {
+	final Buffer fifo = BufferUtils.synchronizedBuffer(new CircularFifoBuffer(200));
+
+	public Log4jMemoryAppender() {
+		System.out.println("hier");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void append(LoggingEvent event) {
+		this.fifo.add(new Entry(event));
+	}
+
+	@Override
+	public void close() {
+		// cleanup buffer
+		this.fifo.clear();
+	}
+
+	@Override
+	public boolean requiresLayout() {
+		return false;
+	}
+
+	public Iterator getLog() {
+		return fifo.iterator();
+	}
+
+	class Entry implements LogEntry {
+		private final LoggingEvent log4jevent;
+
+		public Entry(LoggingEvent log4jevent) {
+			this.log4jevent = log4jevent;
+		}
+
+		public Bundle getBundle() {
+			return null;
+		}
+
+		public Throwable getException() {
+			ThrowableInformation ti = log4jevent.getThrowableInformation();
+			return (ti == null)?null:ti.getThrowable();
+		}
+
+		public int getLevel() {
+			Level level = this.log4jevent.getLevel();
+			switch (level.toInt()) {
+				case Level.FATAL_INT:
+				case Level.ERROR_INT:
+					return LogService.LOG_ERROR;
+	
+				case Level.WARN_INT:
+					return LogService.LOG_WARNING;
+					
+				case Level.INFO_INT:
+					return LogService.LOG_INFO;
+					
+				case Level.DEBUG_INT:
+				case Level.TRACE_INT:
+					return LogService.LOG_DEBUG;
+				
+				default:
+					return LogService.LOG_DEBUG;
+			}
+		}
+
+		public String getMessage() {
+			return this.log4jevent.getMessage().toString();
+		}
+
+		public ServiceReference getServiceReference() {
+			return null;
+		}
+
+		public long getTime() {
+			return this.log4jevent.timeStamp;
+		}
+
+	}
+}
--- bundles/GUI/src/main/java/org/paxle/gui/impl/servlets/LogView.java
+++ bundles/GUI/src/main/java/org/paxle/gui/impl/servlets/LogView.java
@@ -5,14 +5,30 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.apache.velocity.Template;
 import org.apache.velocity.context.Context;
 import org.osgi.service.log.LogService;
 import org.paxle.gui.ALayoutServlet;
+import org.paxle.gui.impl.Log4jMemoryAppender;
+import org.paxle.gui.impl.ServiceManager;
 
 public class LogView extends ALayoutServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	private final Log4jMemoryAppender log4jAppender;
+	
+	public LogView() {
+		// creating a custom appender to intercept log4j events
+		this.log4jAppender = new Log4jMemoryAppender();
+		
+		// getting the Log4j root logger
+		Logger rootLogger = Logger.getRootLogger();
+		
+		// append our custom in memory appender
+		rootLogger.addAppender(log4jAppender);
+	}
 	
 	@Override
 	public Template handleRequest( HttpServletRequest request, HttpServletResponse response, Context context)
@@ -21,21 +37,29 @@ public class LogView extends ALayoutServlet {
 
         try {
         	
-        	if( request.getParameter( "filterLogLevel") != null) {
-        		context.put( "filterLogLevel", new Integer( request.getParameter( "filterLogLevel")));
-        	}
-        	else {
+        	if( request.getParameter("filterLogLevel") != null) {
+        		context.put( "filterLogLevel", new Integer(request.getParameter( "filterLogLevel")));
+        	} else {
         		context.put( "filterLogLevel", Integer.valueOf(4));
         	}
         	
+        	if(request.getParameter("logType") == null || request.getParameter("logType").equals("log4j")) {
+          		context.put("logType", "log4j");
+        		context.put("logReader",this.log4jAppender);
+        	} else {
+        		context.put("logType", request.getParameter( "logType"));
+        		context.put("logReader",((ServiceManager)context.get("manager")).getService("org.osgi.service.log.LogReaderService"));
+  
+        	}
+        	
         	//HashMap to determine LogLevelName
-        	HashMap<Integer, String> level = new HashMap<Integer, String>();
-        	level.put( Integer.valueOf(LogService.LOG_ERROR) , "error");
-        	level.put( Integer.valueOf(LogService.LOG_WARNING), "warning");
-        	level.put( Integer.valueOf(LogService.LOG_INFO), "info");
-        	level.put( Integer.valueOf(LogService.LOG_DEBUG), "debug");
-        	context.put( "logLevelName" , level);
-            
+        	HashMap<Integer, String> logLevelName = new HashMap<Integer, String>();
+        	logLevelName.put(Integer.valueOf(LogService.LOG_ERROR) , "error");
+        	logLevelName.put(Integer.valueOf(LogService.LOG_WARNING), "warning");
+        	logLevelName.put(Integer.valueOf(LogService.LOG_INFO), "info");
+        	logLevelName.put(Integer.valueOf(LogService.LOG_DEBUG), "debug");
+        	context.put( "logLevelNames" , logLevelName);
+        	
             template = this.getTemplate("/resources/templates/LogView.vm");
         } catch( Exception e ) {
           System.err.println("Exception caught: " + e.getMessage());
