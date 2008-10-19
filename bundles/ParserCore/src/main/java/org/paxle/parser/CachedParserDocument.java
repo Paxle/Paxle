481e46603e6eecd4aa1e3e9230e01ce98eac2e70
Franz Brauße <dev@karlchenofhell.org>
2008-10-19 18:46:41 +0000

    - added Paxle licence-header to source-files
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1619 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/ParserCore/src/main/java/org/paxle/parser/CachedParserDocument.java
+++ bundles/ParserCore/src/main/java/org/paxle/parser/CachedParserDocument.java
@@ -1,3 +1,17 @@
+/*
+This file is part of the Paxle project.
+Visit http://www.paxle.net for more information.
+Copyright 2007-2008 the original author or authors.
+
+Licensed under the terms of the Common Public License 1.0 ("CPL 1.0"). 
+Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
+The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt 
+or in the file LICENSE.txt in the root directory of the Paxle distribution.
+
+Unless required by applicable law or agreed to in writing, this software is distributed
+on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*/
+
 package org.paxle.parser;
 
 import java.io.File;
@@ -12,7 +26,7 @@ import org.paxle.parser.iotools.CachedWriter;
 
 public final class CachedParserDocument extends ParserDocument implements IParserDocument {
 	
-	private static final int MAX_TEXT_SIZE_RAM = 2 * 1024 * 1024; // 2 mio. characters == 4 MB
+	private static final int MAX_TEXT_SIZE_RAM = 1 * 1024 * 1024; // 1 mio. characters == 2 MB
 	private CachedWriter text;
 	
 	public CachedParserDocument(ITempFileManager tfm) {
