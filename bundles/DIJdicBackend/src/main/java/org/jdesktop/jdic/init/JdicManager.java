8cc7329f87c392a135152e238c6ed790bfb8487e
Martin Thelian <martin.thelian@gmail.com>
2008-10-20 06:12:30 +0000

    *) reformatting license headers to allow the maven license plugin to detect them (required for mvn license:check and mvn license:remove) See: https://bugs.pxl.li/view.php?id=158
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1629 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/DIJdicBackend/src/main/java/org/jdesktop/jdic/init/JdicManager.java
+++ bundles/DIJdicBackend/src/main/java/org/jdesktop/jdic/init/JdicManager.java
@@ -1,21 +1,15 @@
-/*
- * Copyright (C) 2004 Sun Microsystems, Inc. All rights reserved. Use is
- * subject to license terms.
+/**
+ * This file is part of the Paxle project.
+ * Visit http://www.paxle.net for more information.
+ * Copyright 2007-2008 the original author or authors.
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * Licensed under the terms of the Common Public License 1.0 ("CPL 1.0"). 
+ * Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
+ * The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt 
+ * or in the file LICENSE.txt in the root directory of the Paxle distribution.
  * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA.
+ * Unless required by applicable law or agreed to in writing, this software is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
 package org.jdesktop.jdic.init;
