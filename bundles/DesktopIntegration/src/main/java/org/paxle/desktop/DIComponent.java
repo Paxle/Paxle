278e3782a1dbc5c70d3596be5ce8602bb7f63f7b
Franz Brauße <dev@karlchenofhell.org>
2008-10-21 11:04:51 +0000

    - documented the DIComponent-interface and deprecated two methods one of which will be replaced by another one when I have time to do it...
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1647 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/DesktopIntegration/src/main/java/org/paxle/desktop/DIComponent.java
+++ bundles/DesktopIntegration/src/main/java/org/paxle/desktop/DIComponent.java
@@ -18,11 +18,54 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Frame;
 
+/**
+ * A Swing- or AWT-component that can be registered at the framework which provides
+ * a graphical user-interface. The DesktopIntegration bundle then adds methods to access
+ * this component to the SystemTray- and other menus it provides to access to Paxle's
+ * GUI-elements.
+ */
+// TODO: add a getName()-method instead of getTitle()
 public interface DIComponent {
 	
+	/**
+	 * Releases all resources associated with this component and prepares to close and
+	 * dispose the GUI-elements. This method is called before the frame or parent-component
+	 * the {@link #getContainer() container} this {@link DIComponent} provides is disposed.
+	 */
 	public abstract void close();
+	
+	/**
+	 * Typically a {@link DIComponent} is shown in it's own {@link Frame}. The title of the
+	 * frame is set with the value this method returns.
+	 * @return a title for this {@link DIComponent} which can be displayed as a frame-title.
+	 * @deprecated the title should be set on the {@link Frame} when {@link #setFrame(Frame)}
+	 *             has been invoked by the DesktopBundle
+	 */
+	@Deprecated
 	public abstract String getTitle();
+	
+	/**
+	 * To be able to set an optimal size for the {@link Frame} or component this
+	 * {@link DIComponent}'s {@link #getContainer() container} is shown in, this method is
+	 * being invoked when the parent component is being created by the DesktopIntegration
+	 * bundle.
+	 * @return the requested size for the container, this {@link DIComponent} is placed in
+	 * @deprecated the size of the parent container should be set when {@link #setFrame(Frame)}
+	 *             has been invoked by the DesktopBundle
+	 */
+	@Deprecated
 	public abstract Dimension getWindowSize();
+	
+	/**
+	 * @return the container to display in the parent container or {@link Frame}.
+	 */
 	public abstract Container getContainer();
+	
+	/**
+	 * Invoked by the DesktopIntegration bundle when this {@link DIComponent} is prepared to
+	 * being displayed. The argument is valid until {@link #clone()} has been called on this
+	 * component.
+	 * @param frame the {@link Frame} this component is being displayed in. 
+	 */
 	public abstract void setFrame(Frame frame);
 }
