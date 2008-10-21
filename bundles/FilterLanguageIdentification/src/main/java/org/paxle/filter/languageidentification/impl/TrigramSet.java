1daf352d5e8bce572e5aad9d7a2596abc321e934
Roland Ramthun <mail@roland-ramthun.de>
2008-10-21 16:57:07 +0000

    *) Final batch of headers
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1648 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/FilterLanguageIdentification/src/main/java/org/paxle/filter/languageidentification/impl/TrigramSet.java
+++ bundles/FilterLanguageIdentification/src/main/java/org/paxle/filter/languageidentification/impl/TrigramSet.java
@@ -1,3 +1,17 @@
+/**
+ * This file is part of the Paxle project.
+ * Visit http://www.paxle.net for more information.
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the terms of the Common Public License 1.0 ("CPL 1.0"). 
+ * Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
+ * The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt 
+ * or in the file LICENSE.txt in the root directory of the Paxle distribution.
+ * 
+ * Unless required by applicable law or agreed to in writing, this software is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 package org.paxle.filter.languageidentification.impl;
 
 import java.io.BufferedReader;
@@ -121,7 +135,7 @@ public class TrigramSet {
 			}
 		}
 
-		//jetzt wird der absolute count in ein rangschema geändert
+		//jetzt wird der absolute count in ein rangschema geï¿½ndert
 
 		//sammeln der unterschiedlichen counts
 		Iterator<String> i2 = trigrams.keySet().iterator();
@@ -130,7 +144,7 @@ public class TrigramSet {
 			counts.add(trigrams.get(i2.next()));
 		}
 
-		//der treeset ist sortiert, also kann man jetzt nacheinander einfach die ränge in die neue map eintragen
+		//der treeset ist sortiert, also kann man jetzt nacheinander einfach die rï¿½nge in die neue map eintragen
 		int rang = counts.size();
 		Iterator<Integer> c1 = counts.iterator();
 		HashMap<String, Integer> newmap = new HashMap<String, Integer>();
--- bundles/Paxle/src/main/resources/bin/launchBrowser.sh
+++ bundles/Paxle/src/main/resources/bin/launchBrowser.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+#
+# This file is part of the Paxle project.
+# Visit http://www.paxle.net for more information.
+# Copyright ${inceptionYear}-2008 the original author or authors.
+# 
+# Licensed under the terms of the Common Public License 1.0 ("CPL 1.0"). 
+# Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
+# The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt 
+# or in the file LICENSE.txt in the root directory of the Paxle distribution.
+# 
+# Unless required by applicable law or agreed to in writing, this software is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
 
 # ShellScript 
 # - to detect a desktop and their native
