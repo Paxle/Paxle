b7f32a1a16e84f0d6d33d919d557aaee06e3d81a
Martin Thelian <martin.thelian@gmail.com>
2008-10-24 15:07:46 +0000

    *) Initial import for Optional Luke DI-bundle See: https://bugs.pxl.li/view.php?id=246
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1678 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

---
+++ bundles/DiAddonLuke/src/main/java/org/paxle/desktop/addon/luke/impl/Activator.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of the Paxle project.
+ * Visit http://www.paxle.net for more information.
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the terms of the Common Public License 1.0 ("CPL 1.0"). 
+ * Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
+ * The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt 
+ * or in the file LICENSE.txt in the root directory of the Paxle distribution.
+ * 
+ * Unless required by applicable law or agreed to in writing, this software is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package org.paxle.desktop.addon.luke.impl;
+
+import org.getopt.luke.Luke;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/**
+	 * An instance of Luke
+	 */
+	private Luke luke = null;
+	
+	/**
+	 * This function is called by the osgi-framework to start the bundle.
+	 * @see BundleActivator#start(BundleContext) 
+	 */	
+	public void start(BundleContext bc) throws Exception {
+		this.luke = Luke.startLuke(new String[0]);
+	}
+
+	/**
+	 * This function is called by the osgi-framework to stop the bundle.
+	 * @see BundleActivator#stop(BundleContext)
+	 */		
+	public void stop(BundleContext context) throws Exception {
+		if (this.luke != null) {
+			this.luke.actionClose();
+		}
+	}
+}
