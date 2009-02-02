74ab198895066b0d01b194fa15e406d164083891
Roland Ramthun <mail@roland-ramthun.de>
2009-02-02 19:26:23 +0000

    *) Updated license headers for 2k9
    Paxle/pom.xml doesn't seem to inherit {project.inceptionYear}, which needs to be fixed!
    
    git-svn-id: svn://svn.paxle.net/paxle/trunk@1918 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/Paxle/src/main/resources/equinox/start.sh
+++ bundles/Paxle/src/main/resources/equinox/start.sh
@@ -2,17 +2,16 @@
 #
 # This file is part of the Paxle project.
 # Visit http://www.paxle.net for more information.
-# Copyright ${inceptionYear}-2008 the original author or authors.
-# 
-# Licensed under the terms of the Common Public License 1.0 ("CPL 1.0"). 
+# Copyright ${inceptionYear}-2009 the original author or authors.
+#
+# Licensed under the terms of the Common Public License 1.0 ("CPL 1.0").
 # Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
-# The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt 
+# The full license text is available under http://www.opensource.org/licenses/cpl1.0.txt
 # or in the file LICENSE.txt in the root directory of the Paxle distribution.
-# 
+#
 # Unless required by applicable law or agreed to in writing, this software is distributed
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #
-
 # java runtime lookup
 test \! -z "$JAVA_HOME" && JAVA=$JAVA_HOME/bin/java
 test -z "$JAVA" && JAVA="`which java`"
