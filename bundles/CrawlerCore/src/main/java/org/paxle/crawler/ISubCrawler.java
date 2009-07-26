921fad85245468c306599b8b3bc6b26f7153263b
Martin Thelian <martin.thelian@gmail.com>
2009-07-26 09:57:11 +0000

    *) moving javadoc to proper place
    git-svn-id: svn://svn.paxle.net/paxle/trunk@2481 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/CrawlerCore/src/main/java/org/paxle/crawler/ISubCrawler.java
+++ bundles/CrawlerCore/src/main/java/org/paxle/crawler/ISubCrawler.java
@@ -19,46 +19,47 @@ import org.paxle.core.doc.ICrawlerDocument;
 
 
 /**
- * The interface that must be implemented by all sub-crawlers
+ * The interface that must be implemented by all sub-crawlers.
+ * <hr/>
+ * <i>Usage Example (using BundleContext.registerService(...)):</i>
+ * <pre><code>
+ * public class Activator implements BundleActivator {
+ *    public void start(BundleContext bc) throws Exception {
+ *       // creating the crawler
+ *       ISubCrawler myCrawler = new MyCrawler();
+ * 
+ *       // setting the crawler properties
+ *       Hashtable<String,Object> propsCrawler = new Hashtable<String, Object>();
+ *       propsCrawler.put(Constants.SERVICE_PID, MyCrawler.class.getName());
+ *       propsCrawler.put(ISubCrawler.PROP_PROTOCOL, new String[]{"myProtocol"});	  
+ * 
+ *       // registering the crawler as a service to the framework
+ *       bc.registerService(new String[]{ISubCrawler.class.getName()}, myCrawler, propsCrawler);
+ *    }
+ * }
+ * </code></pre>
+ * <hr/>
+ * <i>Usage Example (using OSGi DS):</i>
+ * <pre><code>
+ * &#064;Component
+ * &#064;Service(ISubCrawler.class)
+ * &#064;Property(name=ISubCrawler.PROP_PROTOCOL, value={"myProtocol"})
+ * public class MyCrawler implements ISubCrawler {
+ * 
+ *    public ICrawlerDocument request(URI location) {
+ *       // do crawling here ...
+ *    }
+ * }
+ * </code></pre>
  */
 public interface ISubCrawler {
 	/*
 	 * A list of service properties
 	 */
 	/** 
-	 * The network protocols supported by the sub-crawler. <br/>The value of this property must
+	 * The network protocols supported by the sub-crawler. <br/>
+	 * The value of this property must
 	 * be of type String[].
-	 * <hr/>
-	 * <i>Usage Example (using BundleContext.registerService(...)):</i>
-	 * <pre><code>
-	 * public class Activator implements BundleActivator {
-	 *    public void start(BundleContext bc) throws Exception {
-	 *       // creating the crawler
-	 *       ISubCrawler myCrawler = new MyCrawler();
-	 * 
-	 *       // setting the crawler properties
-	 *       Hashtable<String,Object> propsCrawler = new Hashtable<String, Object>();
-	 *       propsCrawler.put(Constants.SERVICE_PID, MyCrawler.class.getName());
-	 *       propsCrawler.put(ISubCrawler.PROP_PROTOCOL, new String[]{"myProtocol"});	  
-	 * 
-     *       // registering the crawler as a service to the framework
-	 *       bc.registerService(new String[]{ISubCrawler.class.getName()}, myCrawler, propsCrawler);
-	 *    }
-	 * }
-	 * </code></pre>
-	 * <hr/>
-	 * <i>Usage Example (using OSGi DS):</i>
-	 * <pre><code>
-	 * &#064;Component
-	 * &#064;Service(ISubCrawler.class)
-	 * &#064;Property(name=ISubCrawler.PROP_PROTOCOL, value={"myProtocol"})
-	 * public class MyCrawler implements ISubCrawler {
-	 * 
-	 *    public ICrawlerDocument request(URI location) {
-	 *       // do crawling here ...
-	 *    }
-	 * }
-	 * </code></pre>
 	 */
 	public static final String PROP_PROTOCOL = "Protocol";
 	
