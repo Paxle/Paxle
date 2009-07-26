9e8a0739143fb17aa45edfed5ca34935c314c601
Martin Thelian <martin.thelian@gmail.com>
2009-07-26 09:55:24 +0000

    *) more javadoc
    git-svn-id: svn://svn.paxle.net/paxle/trunk@2480 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/CrawlerCore/src/main/java/org/paxle/crawler/ISubCrawler.java
+++ bundles/CrawlerCore/src/main/java/org/paxle/crawler/ISubCrawler.java
@@ -28,8 +28,8 @@ public interface ISubCrawler {
 	/** 
 	 * The network protocols supported by the sub-crawler. <br/>The value of this property must
 	 * be of type String[].
-	 * <p/>
-	 * <i>Usage Example:</i>
+	 * <hr/>
+	 * <i>Usage Example (using BundleContext.registerService(...)):</i>
 	 * <pre><code>
 	 * public class Activator implements BundleActivator {
 	 *    public void start(BundleContext bc) throws Exception {
@@ -43,6 +43,19 @@ public interface ISubCrawler {
 	 * 
      *       // registering the crawler as a service to the framework
 	 *       bc.registerService(new String[]{ISubCrawler.class.getName()}, myCrawler, propsCrawler);
+	 *    }
+	 * }
+	 * </code></pre>
+	 * <hr/>
+	 * <i>Usage Example (using OSGi DS):</i>
+	 * <pre><code>
+	 * &#064;Component
+	 * &#064;Service(ISubCrawler.class)
+	 * &#064;Property(name=ISubCrawler.PROP_PROTOCOL, value={"myProtocol"})
+	 * public class MyCrawler implements ISubCrawler {
+	 * 
+	 *    public ICrawlerDocument request(URI location) {
+	 *       // do crawling here ...
 	 *    }
 	 * }
 	 * </code></pre>
