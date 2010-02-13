558fccb041f77bbb24485e1cf18fc2864c116bcb
Roland Ramthun <mail@roland-ramthun.de>
2010-02-13 16:57:00 +0000

    *) Update files with 2010 license header
    git-svn-id: svn://svn.paxle.net/paxle/trunk@2617 f5d3ab42-bacf-4b1a-9692-565be65a0cfe

--- bundles/CryptUtils/src/main/java/org/paxle/crypt/impl/md5/MD5Crypt.java
+++ bundles/CryptUtils/src/main/java/org/paxle/crypt/impl/md5/MD5Crypt.java
@@ -1,7 +1,7 @@
 /**
  * This file is part of the Paxle project.
  * Visit http://www.paxle.net for more information.
- * Copyright 2007-2009 the original author or authors.
+ * Copyright 2007-2010 the original author or authors.
  *
  * Licensed under the terms of the Common Public License 1.0 ("CPL 1.0").
  * Any use, reproduction or distribution of this program constitutes the recipient's acceptance of this agreement.
@@ -11,6 +11,7 @@
  * Unless required by applicable law or agreed to in writing, this software is distributed
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
+
 package org.paxle.crypt.impl.md5;
 
 import java.io.InputStream;
